18a072186deb92668df4c52bf7e4b9b17bdf3a8e
PhantomPizzaPie <phantompizzapie@gmail.com>
2026-06-20 12:14:18 +0300

    Added Main Character
    Added Main Character, Added speed indicator, Changed Camera, Changed background

---
+++ Assets/Scripts/MainCharacter.cs
@@ -0,0 +1,22 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class MainCharacter : MonoBehaviour
+{
+    public static MainCharacter instance { get; private set; }
+    private int speed;
+
+    private void Awake()
+    {
+        this.speed = 100;
+        if (instance != null && instance != this)
+            Destroy(this);
+        else
+            instance = this;
+    }
+
+    public int GetSpeed() 
+    {
+        return this.speed;
+    }
+}
