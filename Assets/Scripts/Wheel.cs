6cad9be3c022c58b7b99989d3622e393d2df6c82
PhantomPizzaPie <phantompizzapie@gmail.com>
2026-06-23 00:20:57 +0300

    Merge branch 'dev' of https://github.com/PhantomPizzaPie/SlayTheSpinner into dev

---
+++ Assets/Scripts/Wheel.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using DG.Tweening;
+using Random = UnityEngine.Random;
+
+public class Wheel : MonoBehaviour
+{
+    [SerializeField] float minSpinDuration = 1f;
+    [SerializeField] float maxSpinDuration = 5f;
+    [SerializeField] float maxSpinAngle = 2000;
+    [SerializeField] float minSpinAngle = 1200;
+    public void Spin()
+    {
+        float spinningAngle = Random.Range(minSpinAngle, maxSpinAngle);
+        float spinningDuration = Random.Range(minSpinDuration, maxSpinDuration);
+        transform.DORotate(new Vector3(0, 0, spinningAngle), spinningDuration, RotateMode.FastBeyond360);
+    }
+}
